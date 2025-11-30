9c057f93a136ddbad618514daf73feb88a9d1c1b
Subham Singh <subham0506singh@gmail.com>
2025-11-30 14:10:48 +0530

    implement apiu routes,server actions and caching and the event details page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  cacheComponents:true,
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol:'https',
+        hostname:'res.cloudinary.com',
+      }
+    ]
+  },
   async rewrites() {
     return [
       {
